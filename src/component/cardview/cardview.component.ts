3eb4e8711309f8402ecb9046e8795c087074dd56
신형준 <tronto198@gmail.com>
2020-03-27 16:20:37 +0900

    create cardview component

---
+++ src/component/cardview/cardview.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cardview',
+  templateUrl: './cardview.component.html',
+  styleUrls: ['./cardview.component.scss'],
+})
+export class CardviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
