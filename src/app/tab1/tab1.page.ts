322988dbc41329613aae74c4b629b32a8c74ac62
김민지 <39117025+mjkim103301@users.noreply.github.com>
2020-03-30 22:31:04 +0900

    card view component  추가

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
+  //templateUrl: 'tab1.page.html',
+  templateUrl:'app.cardview.component.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
