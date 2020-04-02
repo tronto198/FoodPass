20ed8fab62f490fb73b1fc2082c586a2b15d15bb
mjkim <mjkim103301@gmail.com>
2020-04-02 16:43:19 +0900

    card view component 1차 완성
    앞으로 카드뷰 비율이랑 내부 구성이랑 반응 완성해야함

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   //templateUrl: 'tab1.page.html',
-  templateUrl:'../../component/cardview.component.html',
+  templateUrl:'../../component/cardview/cardview.component.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
