ff1300b054e59ca700ef0cf84593a840b829f136
Park-new-project <parkmj236@gmail.com>
2020-04-10 23:51:49 +0900

    지도 표시
    -카카오 지도 연결
    (필요시 구글 지도로 변경)
    -위치 권한 요청
    
    -해야 할 것
    권한 허가 시 본인 위치,
    좌표 클릭 시 위치 변경,
    입력 주소로 위치 변경,

--- src/app/modal-pages/map/map.page.ts
+++ src/app/modal-pages/map/map.page.ts
@@ -1,19 +1,77 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NavParams, ModalController } from '@ionic/angular';
 
+// kakao map API
+declare var kakao;
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
   styleUrls: ['./map.page.scss'],
 })
+
 export class MapPage implements OnInit {
+
+  position: any;
+  map: any;
 
   constructor(
 
     private modalCtrl : ModalController
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.position = new kakao.maps.LatLng(36.350456, 127.384818)
+    //지연 후 한번에 그려서 표시 
+      setTimeout(() => {
+        const mapOptions = {
+          center: this.position,
+          level: 3
+        };
+          
+        this.map = new kakao.maps.Map(document.getElementById('map'), mapOptions);
+
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+        
+            // var lat = position.coords.latitude, // 위도
+            //     lon = position.coords.longitude; // 경도
+            
+            // var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+            //     message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+            
+            // 마커와 인포윈도우를 표시합니다
+            //this.displayMarker(locPosition, message);
+                
+          });
+        }
+    
+      }, 300);
+  }
+
+  // displayMarker(locPosition, message) {
+
+  //   // 마커를 생성합니다
+  //   var marker = new kakao.maps.Marker({  
+  //       map: this.map, 
+  //       position: locPosition
+  //   }); 
+    
+  //   var iwContent = message, // 인포윈도우에 표시할 내용
+  //       iwRemoveable = true;
+
+  //   // 인포윈도우를 생성합니다
+  //   var infowindow = new kakao.maps.InfoWindow({
+  //       content : iwContent,
+  //       removable : iwRemoveable
+  //   });
+    
+  //   // 인포윈도우를 마커위에 표시합니다 
+  //   infowindow.open(this.map, marker);
+    
+  //   // 지도 중심좌표를 접속위치로 변경합니다
+  //   this.map.setCenter(locPosition);     
+  // }
 
   dismiss(){
     this.modalCtrl.dismiss();
