b3c5b24b72c4df45a78e6b8245599d629c716b5a
Park-new-project <parkmj236@gmail.com>
2020-04-16 02:13:03 +0900

    지도 연결
     - 카카오 맵
     - 현재 위치 받아오기

--- src/app/modal-pages/map/map.page.ts
+++ src/app/modal-pages/map/map.page.ts
@@ -1,7 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NavParams, ModalController } from '@ionic/angular';
 
-// kakao map API
+import { Plugins } from '@capacitor/core';
+const { Geolocation } = Plugins;
+
+// Kakao Map API
 declare var kakao;
 
 @Component({
@@ -11,67 +14,84 @@ declare var kakao;
 })
 
 export class MapPage implements OnInit {
-
-  position: any;
   map: any;
 
+  position: any;
+  lat: any;
+  lon: any;
+
+  message: any;
+
   constructor(
-
     private modalCtrl : ModalController
-  ) { }
-
+  ) { 
+    // 초기 값 (대전 시청)
+    this.lat = 36.350456;
+    this.lon = 127.384818;
+    this.getCurrentPosition();
+  }
+  
   ngOnInit() {
-    this.position = new kakao.maps.LatLng(36.350456, 127.384818)
-    //지연 후 한번에 그려서 표시 
-      setTimeout(() => {
-        const mapOptions = {
-          center: this.position,
-          level: 3
-        };
-          
-        this.map = new kakao.maps.Map(document.getElementById('map'), mapOptions);
 
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-        
-            // var lat = position.coords.latitude, // 위도
-            //     lon = position.coords.longitude; // 경도
-            
-            // var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-            //     message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
-            
-            // 마커와 인포윈도우를 표시합니다
-            //this.displayMarker(locPosition, message);
-                
-          });
-        }
+    this.watchPosition();
+    this.position = new kakao.maps.LatLng(this.lat, this.lon);
+
+    setTimeout(() => {
+      const mapOptions = {
+        center: this.position,
+        level: 3
+      };
+          
+      this.map = new kakao.maps.Map(document.getElementById('map'), mapOptions);
+      this.message = '<div style="padding:5px;">현재 위치</div>';
+      this.displayMarker(this.position, this.message);
     
-      }, 300);
+    }, 300);
+  }
+  //Component 상태변화에 반응하여 호출
+  ngDoCheck(){
+    this.watchPosition();
+    this.position = new kakao.maps.LatLng(this.lat, this.lon);
+    this.displayMarker(this.position, this.message);
   }
 
-  // displayMarker(locPosition, message) {
+  displayMarker(locPosition, message) {
+    // 마커 생성
+    var marker = new kakao.maps.Marker({  
+        map: this.map, 
+        position: locPosition
+    }); 
+    //인포윈도우 내용
+    var iwContent = message,
+        iwRemoveable = true;
+    //인포윈도우 생성
+    var infowindow = new kakao.maps.InfoWindow({
+        content : iwContent,
+        removable : iwRemoveable
+    });
+    
+    infowindow.open(this.map, marker);
+    this.map.setCenter(locPosition);     
+  }
 
-  //   // 마커를 생성합니다
-  //   var marker = new kakao.maps.Marker({  
-  //       map: this.map, 
-  //       position: locPosition
-  //   }); 
-    
-  //   var iwContent = message, // 인포윈도우에 표시할 내용
-  //       iwRemoveable = true;
+  async getCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.lat = coordinates.coords.latitude.toFixed(6);
+    this.lon = coordinates.coords.longitude.toFixed(6);
+  }
 
-  //   // 인포윈도우를 생성합니다
-  //   var infowindow = new kakao.maps.InfoWindow({
-  //       content : iwContent,
-  //       removable : iwRemoveable
-  //   });
-    
-  //   // 인포윈도우를 마커위에 표시합니다 
-  //   infowindow.open(this.map, marker);
-    
-  //   // 지도 중심좌표를 접속위치로 변경합니다
-  //   this.map.setCenter(locPosition);     
-  // }
+  watchPosition() {
+    const wait = Geolocation.watchPosition({}, (position, err) => {
+    })
+  }
+
+  get getLatitude(){
+    return "lat : "+this.lat;
+  }
+
+  get getLongitude(){
+    return "lon : "+this.lon;
+  }
 
   dismiss(){
     this.modalCtrl.dismiss();
