3f9b80d970aa1bed830a9bf20784b14a798abc20
신형준 <tronto198@gmail.com>
2020-04-16 15:51:17 +0900

    해결중1

--- src/app/tabs/tab-home/foodtruck-list/foodtruck-list.page.ts
+++ src/app/tabs/tab-home/foodtruck-list/foodtruck-list.page.ts
@@ -9,7 +9,7 @@ import { ServerConnecterService } from 'src/app/services/server-connecter/server
   styleUrls: ['./foodtruck-list.page.scss'],
 })
 export class FoodtruckListPage implements OnInit {
-  foodtruckList: string[];
+  foodtruckList: FoodtruckData[];
   location: string[];
   inform: string[];
   wating: string[];
@@ -21,12 +21,12 @@ export class FoodtruckListPage implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.foodtruckList = ["닭발 푸드트럭","와플 푸드트럭","아이스크림 푸드트럭"];
+    //this.foodtruckList = ["닭발 푸드트럭","와플 푸드트럭","아이스크림 푸드트럭"];
     this.location=["A", "B", "C"];
     this.inform=["매운 닭발 있습니다.", "초코맛, 딸기맛 와플 있습니다.", "여러가지 맛 아이스크림이 콘, 컵으로 제공됩니다."];
     this.wating=["3","4","5"];
     this.distance=["100m", "500m", "500m"];
-   //this.foodtruckList = this.serverConnecter.getFoodtruckData();
+    this.foodtruckList = this.serverConnecter.getFoodtruckData();
   }
 
 }
