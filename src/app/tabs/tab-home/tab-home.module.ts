d2a9195f60344dea0a70da7ab3293a031c743ffd
신형준 <tronto198@gmail.com>
2020-04-16 15:37:30 +0900

    오류 해결중

--- src/app/tabs/tab-home/tab-home.module.ts
+++ src/app/tabs/tab-home/tab-home.module.ts
@@ -14,14 +14,11 @@ import { from } from 'rxjs';
 import { TabHomeControllerService } from 'src/app/services/tab-home-controller/tab-home-controller.service';
 import { FoodtruckListPage } from './foodtruck-list/foodtruck-list.page';
 import { FoodtruckInfoPage } from './foodtruck-info/foodtruck-info.page';
-<<<<<<< HEAD
-import { MenuListComponent } from 'src/component/menu-list/menu-list.component';
+import { MenuListComponent } from 'src/app/component/menu-list/menu-list.component';
  
-=======
 import { CardviewComponent } from 'src/app/component/cardview/cardview.component';
 import { MapPage } from 'src/app/modal-pages/map/map.page';
 
->>>>>>> 37492a47073d9181c91bd8e43518cb76fad10575
 @NgModule({
   imports: [
     CommonModule,
