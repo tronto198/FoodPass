3968a6bbde93524f2dbe516765ec31c4d9c255f3
mjkim <mjkim103301@gmail.com>
2020-04-16 15:27:25 +0900

    changes

--- src/app/tabs/tab-home/foodtruck-list/foodtruck-list.page.ts
+++ src/app/tabs/tab-home/foodtruck-list/foodtruck-list.page.ts
@@ -18,15 +18,12 @@ export class FoodtruckListPage implements OnInit {
   ) { }
 
   ngOnInit() {
-<<<<<<< HEAD
     this.foodtruckList = ["닭발 푸드트럭","와플 푸드트럭","아이스크림 푸드트럭"];
     this.location=["A", "B", "C"];
     this.inform=["매운 닭발 있습니다.", "초코맛, 딸기맛 와플 있습니다.", "여러가지 맛 아이스크림이 콘, 컵으로 제공됩니다."];
     this.wating=["3","4","5"];
     this.distance=["100m", "500m", "500m"];
-=======
-    this.foodtruckList = ["hh1","hh2","hh3","hh4","hh1","hh2","hh3","hh4"];
->>>>>>> 37492a47073d9181c91bd8e43518cb76fad10575
+  //  this.foodtruckList = ["hh1","hh2","hh3","hh4","hh1","hh2","hh3","hh4"];
   }
 
   test(ft : string){
