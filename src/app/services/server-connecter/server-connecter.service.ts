45da7c3df4dfb5f8c6936a72f508d0703e932805
mjkim <mjkim103301@gmail.com>
2020-05-05 12:41:12 +0900

    cardview slides

--- src/app/services/server-connecter/server-connecter.service.ts
+++ src/app/services/server-connecter/server-connecter.service.ts
@@ -18,9 +18,9 @@ export class ServerConnecterService {
     // }
     this.FoodtruckDummyData.push({id: 10, name: "닭발집 10", locate:"A", distance:500,inform:"치즈닭발, 무뼈닭발있습니다.", 
     grade:3.5, wating:5, notice:"화요일은 충남대 갑니다" , truckImage:"../assets/icon/foodtruck2.png"});
-    this.FoodtruckDummyData.push({id: 11, name: "와플집 11", locate:"B", distance:600, inform:" 초코와플, 바나나와플, 딸기와플, 빙수 있습니다.",
+    this.FoodtruckDummyData.push({id: 11, name: "와플집 11", locate:"B", distance:600, inform:" 초코와플, 바나나와플, 딸기와플,  빙수",
     grade: 4.5, wating:12, notice:"1명당 대기시간 약 3분 소요됩니다."});
-    this.FoodtruckDummyData.push({id: 12, name: "떡볶이집 12", locate:"C", distance:700, inform:"맵기는 순함, 중간, 매움 있고 튀김도 종류별로 주문할 수 있습니다.",
+    this.FoodtruckDummyData.push({id: 12, name: "떡볶이집 12", locate:"C", distance:700, inform:"떡복이 맵기: 순함, 중간, 매움",
     grade:4, wating:3, notice:"이번주 토요일 엑스포 행사장 갑니다."});
     return this.FoodtruckDummyData;
   }
