c077bd4cbec43afeca0206ae8276104122319985
mjkim <mjkim103301@gmail.com>
2020-05-07 18:20:24 +0900

    delete code
    필요없는 주석 삭제
    메뉴 화면의 푸드트럭 card

--- src/app/component/menu-list/menu-list.component.ts
+++ src/app/component/menu-list/menu-list.component.ts
@@ -7,10 +7,6 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MenuListComponent implements OnInit {
   @Input() menuList: string;
-  @Input() foodtruck_id:number;
-  @Input() foodtruck_name:string;
-  @Input() foodtruck_inform:string;
-  @Input() foodtruck_Grade:number;
 
   @Input() id:number;
   @Input() name:string;
