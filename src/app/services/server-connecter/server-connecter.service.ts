8dd69a2e0947111b084149e755a20dbd41eb9d5c
mjkim <mjkim103301@gmail.com>
2020-05-07 17:05:44 +0900

    Update server-connecter.service.ts
    더미데이터 업데이트

--- src/app/services/server-connecter/server-connecter.service.ts
+++ src/app/services/server-connecter/server-connecter.service.ts
@@ -22,6 +22,8 @@ export class ServerConnecterService {
     grade: 4.5, wating:12, notice:"1명당 대기시간 약 3분 소요됩니다.", src:"../assets/icon/foodtruck2.png"});
     this.FoodtruckDummyData.push({id: 12, name: "떡볶이집 12", locate:"C", distance:700, inform:"떡복이 맵기: 순함, 중간, 매움",
     grade:4, wating:3, notice:"이번주 토요일 엑스포 행사장 갑니다.", src:"../assets/icon/foodtruck3.png"});
+    this.FoodtruckDummyData.push({id: 13, name: "13", locate:"default", distance:0, inform:"짝수 맞추기 위한 카드뷰",
+    grade:0, wating:0, notice:"카드가 홀수일 경우 공간만 차지하게 해야 함"});
     return this.FoodtruckDummyData;
   }
 
