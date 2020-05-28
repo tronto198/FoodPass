5c841224ebd95c37e6ed121915eef145aa92110c
신형준 <tronto198@gmail.com>
2020-05-28 17:58:56 +0900

    설명 추가

--- src/app/services/app-data/data-controller/http-communication.interface.ts
+++ src/app/services/app-data/data-controller/http-communication.interface.ts
@@ -1,15 +1,16 @@
 export interface httpRequest {
     userId : number;
-    data : object
+    data : object;      //reqType의 req~~들
 };
   
   
 export interface httpResponse{
     result : boolean,
-    data : object,
-    reason? : string,
+    data : object,      //reqType의 res~~들
+    reason? : string,   //실패한 이유
 }
   
+//이건 서버용 아님
 export interface httpError{
     data? : object,
     reason : string
