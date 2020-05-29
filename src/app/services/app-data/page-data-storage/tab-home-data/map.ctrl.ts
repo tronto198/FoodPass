b66b90d1091ee22deceb5cfc5ee583a0a35e436e
신형준 <tronto198@gmail.com>
2020-05-29 15:47:23 +0900

    create mapCtrl

---
+++ src/app/services/app-data/page-data-storage/tab-home-data/map.ctrl.ts
@@ -0,0 +1,10 @@
+import { DataControllerService } from '../../data-controller/data-controller.service';
+
+export class TabHomeMapCtrl {
+
+    constructor(private dataCtrl: DataControllerService){
+
+    }
+
+    
+}
