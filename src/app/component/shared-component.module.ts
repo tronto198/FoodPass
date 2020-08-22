5d7734e8b636c1d4ebc1f82db0774f91616ec707
신형준 <tronto980@gmail.com>
2020-08-22 14:35:28 +0900

    에러 수정

--- src/app/component/shared-component.module.ts
+++ src/app/component/shared-component.module.ts
@@ -2,15 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { OrderCardviewComponentModule } from './order-cardview/order-cardview.module';
-import { OrderCardviewComponent } from './order-cardview/order-cardview.component';
 import { TagWaitingComponentModule } from './tag-waiting/tag-waiting.module';
-import { OwnerEditableLabelComponentModule } from './owner-editable-label/owner-editable-label.module';
 import { FtViewComponentModule } from './ft-view/ft-view.module';
 import { FtViewComponent } from './ft-view/ft-view.component';
-import { BtnBasketComponent } from './btn-basket/btn-basket.component';
 import { FtWaitingViewComponent } from './ft-waiting-view/ft-waiting-view.component';
-import { FtBasketViewComponent } from './ft-basket-view/ft-basket-view.component';
 import { FtOrderhistoryViewComponent } from './ft-orderhistory-view/ft-orderhistory-view.component';
 import { FtMenuComponent } from './ft-menu/ft-menu.component';
 import { FtMenuComponentsModule } from './ft-menu/ft-menu.module';
@@ -19,16 +14,17 @@ import { UserAccountViewComponent } from './user-account-view/user-account-view.
 import { OwnerAccountViewComponent } from './owner-account-view/owner-account-view.component';
 import { OwnerAccountViewModule } from './owner-account-view/owner-account-view.module';
 import { OwnerCookingViewComponent } from './owner-cooking-view/owner-cooking-view.component';
-import { OwnerCookingViewModule } from './owner-cooking-view/owner-cooking-view.module';
-import { FtImgComponent } from './ft-img/ft-img.component';
 import { FtImgComponentModule } from './ft-img/ft-img.module';
+import { OwnerEditableLabelComponentModule } from './owner-editable-label/owner-editable-label.module';
+import { OwnerCookingViewModule } from './owner-cooking-view/owner-cooking-view.module';
+import { BtnBasketComponent } from './btn-basket/btn-basket.component';
+import { FtBasketViewComponent } from './ft-basket-view/ft-basket-view.component';
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
         IonicModule,
-        FtImgComponentModule,
         TagWaitingComponentModule,
         OwnerEditableLabelComponentModule,
         FtViewComponentModule,
@@ -43,18 +39,15 @@ import { FtImgComponentModule } from './ft-img/ft-img.module';
         BtnBasketComponent,
         FtWaitingViewComponent,
         FtBasketViewComponent,
-        FtOrderhistoryViewComponent
-
+        FtOrderhistoryViewComponent,
+        UserAccountViewComponent,
+        
     ],
     declarations: [
         BtnBasketComponent,
         FtWaitingViewComponent,
         FtBasketViewComponent,
         FtOrderhistoryViewComponent,
-        FtMenuComponent,
-        UserAccountViewComponent,
-        OwnerAccountViewComponent,
-        OwnerCookingViewComponent
 
     ],
   
